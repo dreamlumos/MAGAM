f87bbc5a449c50caf4afe4bd7b557982fa21a74c
3803025 <kiara.gigacz@etu.upmc.fr>
2022-04-27 13:36:20 +0200

    BKT

--- src/bkttest.py
+++ src/bkttest.py
@@ -2,52 +2,15 @@ from multiprocessing import freeze_support
 
 from pyBKT.models import Model
 
+from models.BKTData import *
+
 
 if __name__ == '__main__':
-    freeze_support()
-    # Initialize the model with an optional seed
-    model = Model(seed = 42, num_fits = 1)
+    # freeze_support()
+    
+    defaults = {'user_id': 'User', 'skill_name': 'KC', 'correct': 'IsCorrect'}   
+    # model.fit(data_path = '../data/eedi_data/quizzing-policy-main/neurips_challenge/train_task_3_4_with_concept.csv', defaults=defaults)
 
-    # Fetch Assistments and CognitiveTutor data (optional - if you have your own dataset, that's fine too!)
-    # model.fetch_dataset('https://raw.githubusercontent.com/CAHLR/pyBKT-examples/master/data/as.csv', '.')
-    # model.fetch_dataset('https://raw.githubusercontent.com/CAHLR/pyBKT-examples/master/data/ct.csv', '../data/bkt_data')
-
-    # Train a simple BKT model on all skills in the CT dataset
-    #model.fit(data_path = 'ct.csv')
-    defaults = {'user_id': 'UserId', 'skill_name': 'Concept', 'correct': 'IsCorrect'}   
-    model.fit(data_path = '../data/eedi_data/quizzing-policy-main/neurips_challenge/train_task_3_4_with_concept.csv', defaults=defaults)
-
-    # # Train a simple BKT model on one skill in the CT dataset
-    # # Note that calling fit deletes any previous trained BKT model!
-    # model.fit(data_path = 'ct.csv', skills = "Plot imperfect radical")
-
-    # # Train a simple BKT model on multiple skills in the CT dataset
-    # model.fit(data_path = 'ct.csv', skills = ["Plot imperfect radical",
-    #                                           "Plot pi"])
-
-    # # Train a multiguess and slip BKT model on multiple skills in the
-    # # CT dataset. Note: if you are not using CognitiveTutor or Assistments
-    # # data, you may need to provide a column mapping for the guess/slip
-    # # classes to use (i.e. if the column name is gsclasses, you would
-    # # specify multigs = 'gsclasses' or specify a defaults dictionary
-    # # defaults = {'multigs': 'gsclasses'}).
-    # model.fit(data_path = 'ct.csv', skills = ["Plot imperfect radical",
-    #                                           "Plot pi"],
-    #                                 multigs = True)
-
-    # # We can combine multiple model variants.
-    # model.fit(data_path = 'ct.csv', skills = ["Plot imperfect radical",
-    #                                           "Plot pi"],
-    #                                 multigs = True, forgets = True,
-    #                                 multilearn = True)
-
-    # # We can use a different column to specify the different learn and 
-    # # forget classes. In this case, we use student ID.
-    # model.fit(data_path = 'ct.csv', skills = ["Plot imperfect radical",
-    #                                           "Plot pi"],
-    #                                 multigs = True, forgets = True,
-    #                                 multilearn = 'Anon Student Id')
-
-    # View the trained parameters!
-    print(model.params())
-
+    #d = BKTData('../data/eedi_data/quizzing-policy-main/neurips_challenge/train_task_3_4_with_concept.csv', model_defaults=defaults)
+    d = BKTData('../data/bkt_input_2.csv', model_defaults=defaults)
+    d.save_preds_to_csv()
--- src/models/BKTData.py
+++ src/models/BKTData.py
@@ -1,4 +1,71 @@
+from pyBKT.models import Model
+import pandas as pd
+import numpy as np
+
+
 class BKTData():
 
-	def __init__(self, training_data):
-		pass
+	def __init__(self, training_data_path, model_seed=42, model_defaults=None):
+
+		self.training_data_path = training_data_path
+
+		if model_defaults == None:
+			self.model_defaults = {'user_id': 'user_id', 'skill_name': 'skill_name', 'correct': 'correct'}
+		else:
+			self.model_defaults = model_defaults
+
+		self.model = Model(seed = model_seed, num_fits = 1, defaults = model_defaults)
+		self.train()
+
+	def train(self):
+		self.model.fit(self.training_data_path)
+
+	def predict(self, data_path=None):
+
+		if data_path == None:
+			data_path = self.training_data_path
+
+		preds_df = self.model.predict(data_path = data_path)
+		preds_np = np.array(preds_df)
+
+		user_id_column_name = self.model_defaults["user_id"]
+		user_id_column_index = preds_df.columns.get_loc(user_id_column_name)
+		concepts_column_name = self.model_defaults["skill_name"]
+		concepts_column_index = preds_df.columns.get_loc(concepts_column_name)
+
+		self.user_ids = np.unique(preds_np[:, user_id_column_index]) # list of user_id
+		self.concepts = np.unique(preds_np[:, concepts_column_index]) # list of concepts
+
+		all_knowledge_states = []
+		for id in self.user_ids:
+			preds_id = preds_df[preds_df[user_id_column_name] == id]
+			preds_id_np = np.flipud(np.array(preds_id)) # reverse chronological order (last activity done being on top)
+			knowledge_states = []
+			for c in self.concepts:
+				i = 0
+				while i < len(preds_id_np) and preds_id_np[i, concepts_column_index] != c: # looking for the last activity done by this student for concept c
+					i += 1
+				if i >= len(preds_id_np): # student has not done an activity with this concept
+					knowledge_states.append(0.5)
+				else:
+					knowledge_states.append(preds_id_np[i, -1]) # append the most recent state prediction
+
+			all_knowledge_states.append(knowledge_states)
+
+		self.users_predicted_knowledge = np.array(all_knowledge_states).T
+	
+	def get_users_predicted_knowledge(self):
+		if not(hasattr(self, "users_predicted_knowledge")):
+			self.predict()
+		
+		return self.users_predicted_knowledge
+
+	def save_preds_to_csv(self, path="./predictions.csv"):
+		if not(hasattr(self, "users_predicted_knowledge")):
+			self.predict()
+		
+		final_df = pd.DataFrame(self.users_predicted_knowledge, index=self.concepts)
+		final_df.sort_index(axis=0)
+		final_df.sort_index(axis=1)
+
+		return final_df.to_csv(path, header=self.user_ids)
